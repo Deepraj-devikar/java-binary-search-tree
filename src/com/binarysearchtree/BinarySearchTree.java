ec295e6e7b8b975d7b8c364c6abf8fccefaba519
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-09-20 20:19:30 +0530

    ADDED: search functionality to binary search tree then search specific value

--- src/com/binarysearchtree/BinarySearchTree.java
+++ src/com/binarysearchtree/BinarySearchTree.java
@@ -3,12 +3,34 @@ package com.binarysearchtree;
 public class BinarySearchTree <Type extends Comparable> {
 	INode<Type> root;
 	
+	/**
+	 * add key value to binary search tree by wrapping key into the INode 
+	 * first it will check binary search tree is empty or not if it is empty 
+	 * then new node will add to root node as first node into the binary search tree
+	 * otherwise it will add new node below root node 
+	 * 
+	 *  now it will check new node key is less than root node key or greater than root node key 
+	 *  	if it is less than root node key then will add new node to left tree of root node
+	 *  	if it is greater than root node key then will add new node to right tree of root node
+	 *  
+	 * @param key
+	 */
 	public void add(Type key) {
 		INode<Type> newNode = new INode<Type>(key);
 		if(isEmpty()) root = newNode;
 		else addBelowNode(root, newNode);
 	}
 	
+	/**
+	 * Will recursively check for the correct position new node and add new node to that correct position of node
+	 * 
+	 * now it will check new node key is less than current node key or greater than current node key 
+	 *  	if it is less than current node key then will add new node to left tree of current node
+	 *  	if it is greater than current node key then will add new node to right tree of current node
+	 *  
+	 * @param current node
+	 * @param new node
+	 */
 	private void addBelowNode(INode currentNode, INode newNode) {
 		if(currentNode.key.compareTo(newNode.key) < 0) 
 			// new node key is greater than current node key so add new node to right side of current node
@@ -24,6 +46,39 @@ public class BinarySearchTree <Type extends Comparable> {
 		return root == null;
 	}
 	
+	/**
+	 * Search node with the key given is present in binary search tree or not
+	 * 
+	 * @param key
+	 * @return key found in tree then true else false
+	 */
+	public boolean search(Type key) {
+		if(root == null) return false;
+		else return searchKeyInSubTree(root, key);
+	}
+	
+	/**
+	 * recursively check key is found in subtree or not 
+	 * if found then return true if not found then at last return false
+	 * 
+	 * @param currentNode
+	 * @param key
+	 * @return key found in subtree then true else false
+	 */
+	private boolean searchKeyInSubTree(INode<Type> currentNode, Type key) {
+		if(currentNode == null) return false;
+		if(currentNode.key == key) return true;
+		if(searchKeyInSubTree(currentNode.left, key)) return true;
+		return searchKeyInSubTree(currentNode.right, key);
+	}
+	
+	/**
+	 * to correctly add new value to given array by checking where we got null then their add new value to the array
+	 * 
+	 * @param arr
+	 * @param key
+	 * @return array of keys
+	 */
 	private Type[] properlyAddToArray(Type[] arr, Type key) {
 		for (int index = 0; index < arr.length; index++)
 			if(arr[index] == null) {
@@ -33,18 +88,43 @@ public class BinarySearchTree <Type extends Comparable> {
 		return arr;
 	}
 	
+	/**
+	 * pre-order sequence of binary search tree with pre-order traversal
+	 * 
+	 * @param result
+	 * @return result by adding every nodes values to result 
+	 */
 	public Type[] preOrder(Type[] result) {
 		return preOrderTraversel(root, result);
 	}
 	
+	/**
+	 * in-order sequence of binary search tree with in-order traversal
+	 * 
+	 * @param result
+	 * @return result by adding every nodes values to result 
+	 */
 	public Type[] inOrder(Type[] result) {
 		return inOrderTraversel(root, result);
 	}
 	
+	/**
+	 * post-order sequence of binary search tree with post-order traversal
+	 * 
+	 * @param result
+	 * @return result by adding every nodes values to result 
+	 */
 	public Type[] postOrder(Type[] result) {
 		return postOrderTraversel(root, result);
 	}
 	
+	/**
+	 * recursively arrange all nodes values to result array according to pre-order-traversal manner
+	 * 
+	 * @param currentNode
+	 * @param result
+	 * @return result by adding every nodes values to result 
+	 */
 	private Type[] preOrderTraversel(INode<Type> currentNode, Type[] result) {
 		if(currentNode != null) properlyAddToArray(result, currentNode.key);
 		if(currentNode.left != null) result = preOrderTraversel(currentNode.left, result);
@@ -52,6 +132,13 @@ public class BinarySearchTree <Type extends Comparable> {
 		return result;
 	}
 	
+	/**
+	 * recursively arrange all nodes values to result array according to in-order-traversal manner
+	 * 
+	 * @param currentNode
+	 * @param result
+	 * @return result by adding every nodes values to result 
+	 */
 	private Type[] inOrderTraversel(INode<Type> currentNode, Type[] result) {
 		if(currentNode.left != null) result = inOrderTraversel(currentNode.left, result);
 		if(currentNode != null) properlyAddToArray(result, currentNode.key);
@@ -59,6 +146,13 @@ public class BinarySearchTree <Type extends Comparable> {
 		return result;
 	}
 	
+	/**
+	 * recursively arrange all nodes values to result array according to post-order-traversal manner
+	 * 
+	 * @param currentNode
+	 * @param result
+	 * @return result by adding every nodes values to result 
+	 */
 	private Type[] postOrderTraversel(INode<Type> currentNode, Type[] result) {
 		if(currentNode.left != null) result = postOrderTraversel(currentNode.left, result);
 		if(currentNode.right != null) result = postOrderTraversel(currentNode.right, result);
@@ -66,12 +160,23 @@ public class BinarySearchTree <Type extends Comparable> {
 		return result;
 	}
 	
+	/**
+	 * count of nodes in binary search tree
+	 * 
+	 * @return count of nodes in binary search tree
+	 */
 	public int size() {
 		int count = 0;
 		if(root != null) count += sizeOfSubtree(root);
 		return count;
 	}
 	
+	/**
+	 * recursively calculate size of subtries
+	 * 
+	 * @param currentNode
+	 * @return size of subtree
+	 */
 	private int sizeOfSubtree(INode<Type> currentNode) {
 		int count = 0;
 		if(currentNode != null) {
@@ -124,6 +229,12 @@ public class BinarySearchTree <Type extends Comparable> {
 				first = !first;
 			}
 			else System.out.print(", "+key);
+		System.out.println("");
+		if(binarySearchTree.search(63)) {
+			System.out.println("63 found in binary search tree");
+		} else {
+			System.out.println("63 not found in binary search tree");
+		}
 	}
 
 }
